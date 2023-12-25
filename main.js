93d836e6a001cfbd2b1d01e9b5e4004cf021c91d
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2023-12-25 15:43:13 -0500

    Choose your own adventure: entire meal functionality complete

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //ARRAYS WITH FOOD ITEMS:
 
-var sides = ['Mashed Potatoes', 'Mac and Cheese', 'French Fries']
+var sides = ['Mashed Potatoes', 'Mac and Cheese', 'French Fries', 'Tater Tots', 'Salad', 'Soup', 'Calamari']
 
-var desserts = ['Cake', 'Brownies', 'Pie']
+var desserts = ['Chocolate Cake', 'Brownies', 'Apple Pie', 'Ice-cream', 'Key Lime Pie', 'Cheesecake', 'Banana Pudding', 'Tiramisu']
 
-var mains = ['Steak', 'Chicken', 'Pork Roast']
+var mains = ['Steak', 'Chicken', 'Pork Roast', 'Salmon', 'Spaghetti', 'Kabob', 'Tilapia', 'Veggie Stirfry', 'Scallops']
 
 //HTML ELEMENTS:
 
@@ -12,6 +12,7 @@ var box2 = document.querySelector('.box2')
 var side = document.querySelector('#side')
 var main = document.querySelector('#main')
 var dessert = document.querySelector('#dessert')
+var entireMeal = document.querySelector('#entire-meal')
 var blueButton = document.querySelector('.blue-button')
 
 //EVENT LISTENERS:
@@ -27,6 +28,13 @@ function getRandomInt(array) {
 
 //this function displays random food item 
 function chooseFood() {
+  if (entireMeal.checked) {
+    box2.innerHTML = `<div class="dish"> 
+                        <i>You should make:</i> 
+                        <p class="dish-name">${mains[getRandomInt(mains)]} with a side of ${sides[getRandomInt(sides)]} and ${desserts[getRandomInt(desserts)]} for dessert!</p>
+                        <button class="clear-entire">CLEAR</button>
+                    </div>`
+      }
   if (main.checked) {
     box2.innerHTML = `<div class="dish"> 
                         <i>You should make:</i> 
